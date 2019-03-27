5708f39d1ba078cc0fc4c18a87810d53decbb861
Nelson Wu <you.wu@postmates.com>
2019-03-27 16:00:36 -0700

    add channels logic

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,13 +3,8 @@ package ircserver
   * Created by fujiko on 2017/12/22.
   */
 
-import java.net.InetSocketAddress
-
 import Actors.Server
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-import akka.io.Tcp.Write
-import akka.io.{IO, Tcp}
-import akka.util.ByteString
+import akka.actor.{ActorSystem, Props}
 
 object Globals {
   val serverName = "localhost"
